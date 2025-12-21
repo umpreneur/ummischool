d4ce159659050d70c807503a45cd229383c4dbbe
umpreneur <yesrahmahere@gmail.com>
2025-12-22 00:43:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -21,6 +21,8 @@ const blogPreviews = [
   excerpt:
     "Een dagstart is een moment om samen de dag te openen, verwachtingen af te stemmen en ruimte te geven aan wat nodig is op dat moment.",
   slug: "Waarom-een-dagstart?",
+    thumbnailUrl: "https://umpreneur.github.io/ummischool/blog3img.png",
+    imageUrl: "https://umpreneur.github.io/ummischool/blog3img.png",
   },
 {
   id: "4",
@@ -91,6 +93,8 @@ headerImageUrl: "https://umpreneur.github.io/ummischool/blog1img.png",
      "Waarom-een-dagstart?": {
     title: "Waarom een dagstart?",
        category: "Reflectie",
+       headerImageUrl: "https://umpreneur.github.io/ummischool/blog1img.png",
+    imageUrl: "https://umpreneur.github.io/ummischool/blog1img.png",
  content: [
     "Een dagstart is een moment aan het begin van de dag om samen stil te staan bij <strong>wat er komt</strong> en <strong>hoe iedereen erbij zit</strong>. In verschillende vormen van onderwijs wordt dit gebruikt om de dag gezamenlijk te beginnen.",
     "Ook binnen thuisonderwijs kan een dagstart een plek hebben. Hoe dit moment eruitziet, ligt niet vast en verschilt per gezin en per kind.",
