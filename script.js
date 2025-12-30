913ec197c0a5566482d6822562e9ba40064555f2
umpreneur <yesrahmahere@gmail.com>
2025-12-31 00:06:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -194,6 +194,11 @@ content: [
   ]
 },
 
+  "Inhoud-dagstart": {
+  title: "Inhoud dagstart",
+  category: "Thuisonderwijs",
+  headerImageUrl: "https://umpreneur.github.io/ummischool/blog6img.png",
+  imageUrl: "https://umpreneur.github.io/ummischool/blog6img.png",
  content: [
     "Een dagstart begint met even samen zijn. Je gaat samen zitten en praat kort met elkaar. Door te praten leert je kind woorden geven aan wat het voelt en denkt, en aan wat de dag gaat brengen.",
     
